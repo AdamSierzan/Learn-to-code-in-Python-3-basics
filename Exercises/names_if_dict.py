d7417b16ea6c2e24022cb21d54443c41d5ba3b93
adagioo1993 <adagioo1993@gmail.com>
2020-04-03 21:57:32 +0200

    daily update

--- Exercises/names_if_dict.py
+++ Exercises/names_if_dict.py
@@ -1,25 +1,27 @@
-names{"malen":"Adam",
- "Tom", 
- "Jack"]}
-female_names = ["Jackie", "Jenny", "Mary"]
-
-
+names = {
+"Adam" : "male", 
+"Tom" : "male", 
+"Jack" : "male", 
+"Tim" : "male", 
+"Jenny" : "female", 
+"Mary" : "female",
+"Tina" : "female", 
+"Juliet" : "female"
+}
 
 name = input("Type your name: ")
-if name in male_names:
+if (name) in (names.keys()):
     print("Seems like we have your name on the list.")
-elif name in female_names:
-    print("Seems like we have your name on the list :)")
+    print("And its a: ", names[name], "name")
 else:
     print("Sorry, we dont have your name on the list")
     x = input("Do you want to add it to the list, type 'yes' or 'no': ")
     if x == "yes": 
         y = input("Is that a male name? If it is type 'yes': ")
         if y == "yes":
-            male_names.append(name)
+            names[name] = "male"
         else:
-            female_names.append(name)``
+            names[name] = "female"
     if x != "yes":
         print("Ok, that,s cool")
-print(male_names)
-print(female_names)
+print(names)
