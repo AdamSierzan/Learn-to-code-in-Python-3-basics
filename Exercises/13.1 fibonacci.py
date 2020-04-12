e1842f89b59e0516a6b8005706cfd8b5bbb70667
adagioo1993 <adagioo1993@gmail.com>
2020-04-12 21:48:52 +0200

    daily update

--- Exercises/13.1 fibonacci.py
+++ Exercises/13.1 fibonacci.py
@@ -3,9 +3,9 @@
 #0, 1, 1, 2, 3, 5, 8, 13, 21, ....
 #Every next number is found by adding up the two numbers before it.
 #Expected Output : 1 1 2 3 5 8 13 21 34
-lenght = int(input("How many fibonacci series you want"))
+length = int(input("How many fibonacci series you want"))
 n = [1,1]
-while len(n) < lenght:
+while len(n) < length:
     x = len(n[:-1]) + len(n[:-2])
     n.append(x)
 print(n)
---
+++ Exercises/iterating integers.py
@@ -0,0 +1,22 @@
+#Write a Python program which iterates the integers from 1 to 50. For multiples of three print "Fizz" 
+# instead of the number and for the multiples of five print "Buzz". 
+# For numbers which are multiples of both three and five print "FizzBuzz".
+#Sample Output :
+#fizzbuzz
+#1
+#2
+#fizz
+#4
+#buzz
+
+
+a = list(range(1,50))
+for x in a:
+    if x%5 == 0:
+        print("fizz")
+    elif x%3 == 0:
+        print("buzz")
+    elif x%5 == 0 and x%3 == 0:
+        print("fizz & buzz")
+    else:
+        print(x)
