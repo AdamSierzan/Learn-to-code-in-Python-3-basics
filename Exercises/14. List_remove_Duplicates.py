b353cccf5c701c83b1b921353dbf73288998604f
adagioo1993 <adagioo1993@gmail.com>
2020-04-14 22:10:21 +0200

    daily update

---
+++ Exercises/14. List_remove_Duplicates.py
@@ -0,0 +1,13 @@
+#Write a program (function!) that takes a list and returns a new list that 
+#contains all the elements of the first list minus all the duplicates.
+#
+#Extras:
+#Write two different functions to do this - one using a loop and 
+#constructing a list, and another using sets.
+#Go back and do Exercise 5 using sets, and write the solution for that in a different function.
+
+new_list = [2,3,2,3,4,5,2,3,4,2,3,5,6,7,8]
+print(list(set(new_list)))
+
+
+
---
+++ Exercises/Inch_to_cm_conventer.py
@@ -0,0 +1,3 @@
+cm = [10,20,30]
+inch = [(1.54*num) for num in cm]
+print(inch)
